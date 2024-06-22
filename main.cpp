89cdc32793f235dcaa5e8e8d6c271d3a28f71522
Osemudiamen Itua <osemudiamenitua@gmail.com>
2024-06-22 22:58:16 +0100

    feat: complete jsonnode class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "json.h"
 
-int main(){
-   ose4g::jsonpp::JSONNode json = ose4g::jsonpp::JSONObjectNode();
-   json["name"] = ose4g::jsonpp::JSONStringValue("name");
+int main()
+{
+    JSON json = JSON(JSONType::OBJECT);
+    json["name"] = "ose4g";
+    json["age"] = 24;
+    json["height"] = 5.11;
+    json["hobbies"] = std::vector<JSON>{"Gym", "Anime"};
+    json["enemies"] = nullptr;
+    json["valid"] = true;
+    json["school"] = JSON(JSONType::OBJECT);
+    json["school"]["name"] = "UNILAG";
+
+    std::string name = json["name"];
+    std::cout << std::string(name) << " | " << json["hobbies"][1].get<std::string>() << " | " << std::endl;
 }
